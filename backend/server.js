5c6cf918faf0db5983a953d7e743fe8142057247
Komakula-pooja <poojithakomakula21@gmail.com>
2024-12-21 21:48:44 +0530

    added

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,13 @@ import ProductRoutes from "./routes/product.route.js"
 
 dotenv.config();
 const app = express();
-app.use(cors());
+
+app.use(cors({
+    origin: 'https://shelf-space-fe.vercel.app',  // Adjust this with your frontend's domain
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],   // Allow these HTTP methods
+    allowedHeaders: ['Content-Type', 'Authorization'] // Add other headers as needed
+  }));
+
 const PORT = process.env.PORT || 5000;
 
 const __dirname = path.resolve();
